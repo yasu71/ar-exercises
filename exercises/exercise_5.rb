86dcd8d76414beadb9386ca9ac88cdf3fba078f7
Yasu <yasu71@gmail.com>
2020-04-28 20:17:32 +0000

    Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+totalRevenue = Store.sum("annual_revenue")
+puts "Total Revenue: #{totalRevenue}"
+
+averageRevenue = Store.average("annual_revenue")
+puts "Average Annual Revenue: #{averageRevenue}"
+
+output = Store.where("annual_revenue <= ?", [1000000]).count
+
+puts "the number of stores generating $1M or more in annual sales: #{output}"
